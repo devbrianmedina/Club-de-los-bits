f3e607629670b8498446a38b3f79ba4df13e4b6c
Brian Medina <38367679+devbrianmedina@users.noreply.github.com>
2023-02-22 11:06:14 -0600

    Create main.cpp

---
+++ plantillas/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Incluye la biblioteca de entrada y salida estándar
+using namespace std; // Indica que se utilizarán los elementos de la biblioteca estándar en el espacio de nombres "std"
+
+int main() { // Función principal del programa
+  
+  // TU CODIGO ...
+  
+  return 0; // Indica que el programa finalizó con éxito
+}
