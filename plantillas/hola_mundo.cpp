849359872a4b9b38d3c64d54402337a2317195d3
Brian Medina <38367679+devbrianmedina@users.noreply.github.com>
2023-02-22 11:05:17 -0600

    Create hola_mundo.cpp

---
+++ plantillas/hola_mundo.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Incluye la biblioteca de entrada y salida estándar
+using namespace std; // Indica que se utilizarán los elementos de la biblioteca estándar en el espacio de nombres "std"
+
+int main() { // Función principal del programa
+  cout << "Hola mundo!" << endl; // Imprime el mensaje "Hola mundo!" en la consola
+  return 0; // Indica que el programa finalizó con éxito
+}
