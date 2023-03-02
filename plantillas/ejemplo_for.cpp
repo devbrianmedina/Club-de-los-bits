723b2fd4149b5be1c71b676b85cab656cfb06141
Brian Medina <38367679+devbrianmedina@users.noreply.github.com>
2023-03-01 21:00:02 -0600

    Create ejemplo_for.cpp

---
+++ plantillas/ejemplo_for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Se inicia el for con una variable i inicializada en 1
+    // Se especifica que el for se ejecutará mientras i sea menor o igual a 10
+    // En cada iteración, i se incrementará en 1
+    for (int i = 1; i <= 10; i++) {
+        // Se imprime el valor actual de i en la consola
+        cout << i << endl;
+    }
+    // Se devuelve 0 para indicar que el programa ha finalizado correctamente
+    return 0;
+}
