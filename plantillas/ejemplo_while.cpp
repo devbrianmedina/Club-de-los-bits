50c41c755ace35242f2f7a57b0159ccbbef1ba07
Brian Medina <38367679+devbrianmedina@users.noreply.github.com>
2023-03-01 21:01:16 -0600

    Create ejemplo_while.cpp

---
+++ plantillas/ejemplo_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    // El bucle while se ejecutará mientras i sea menor o igual a 10
+    while (i <= 10) {
+        // Se imprime el valor actual de i en la consola
+        cout << i << endl;
+        // Se incrementa i en 1 en cada iteración
+        i++;
+    }
+    // Se devuelve 0 para indicar que el programa ha finalizado correctamente
+    return 0;
+}
